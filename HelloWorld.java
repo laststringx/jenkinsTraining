99f16804b0fea0083126bc7a041608d208366f49
laststringx <laststring00@gmail.com>
2018-08-12 11:23:46 +0530

    Update HelloWorld.java
    added a few lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ class HelloWorld{
 public static void main(String args[]){
 System.out.println("Hello World!");
 System.out.println("Hello Jenkins!");
+System.out.println("Hello TCS!");
+System.out.println("Hello TDC!");
+ 
   
 }
 }
